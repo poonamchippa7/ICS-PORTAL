ab74ceec0c1f510d081d2fb009da9c3ed807d50c
Poonam Shrinivas Chippa <poonamchippa317@gmail.com>
2022-06-29 22:14:20 +0530

    changes

---
+++ src/components/Anex6.js
@@ -0,0 +1,15 @@
+import React from "react";
+function App(){
+    return(
+
+    <div className={"FORMAT FOR DEVELOPING ICS"}>
+    <h1> FORMAT FOR DEVELOPING ICS INTERNAL STANDARDS IN LOCAL LANGUAGES 
+        </h1>
+      
+      </div>
+        
+
+ );
+}
+
+export default App;
